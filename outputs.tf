18344d8186eab463b3d8c5ad6862cccab0ad21ba
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:15:21 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,49 +1,45 @@
-output "api_management_api_operations" {
-  description = "All api_management_api_operation resources"
-  value       = azurerm_api_management_api_operation.api_management_api_operations
-}
 output "api_management_api_operations_api_management_name" {
-  description = "List of api_management_name values across all api_management_api_operations"
-  value       = [for k, v in azurerm_api_management_api_operation.api_management_api_operations : v.api_management_name]
+  description = "Map of api_management_name values across all api_management_api_operations, keyed the same as var.api_management_api_operations"
+  value       = { for k, v in azurerm_api_management_api_operation.api_management_api_operations : k => v.api_management_name }
 }
 output "api_management_api_operations_api_name" {
-  description = "List of api_name values across all api_management_api_operations"
-  value       = [for k, v in azurerm_api_management_api_operation.api_management_api_operations : v.api_name]
+  description = "Map of api_name values across all api_management_api_operations, keyed the same as var.api_management_api_operations"
+  value       = { for k, v in azurerm_api_management_api_operation.api_management_api_operations : k => v.api_name }
 }
 output "api_management_api_operations_description" {
-  description = "List of description values across all api_management_api_operations"
-  value       = [for k, v in azurerm_api_management_api_operation.api_management_api_operations : v.description]
+  description = "Map of description values across all api_management_api_operations, keyed the same as var.api_management_api_operations"
+  value       = { for k, v in azurerm_api_management_api_operation.api_management_api_operations : k => v.description }
 }
 output "api_management_api_operations_display_name" {
-  description = "List of display_name values across all api_management_api_operations"
-  value       = [for k, v in azurerm_api_management_api_operation.api_management_api_operations : v.display_name]
+  description = "Map of display_name values across all api_management_api_operations, keyed the same as var.api_management_api_operations"
+  value       = { for k, v in azurerm_api_management_api_operation.api_management_api_operations : k => v.display_name }
 }
 output "api_management_api_operations_method" {
-  description = "List of method values across all api_management_api_operations"
-  value       = [for k, v in azurerm_api_management_api_operation.api_management_api_operations : v.method]
+  description = "Map of method values across all api_management_api_operations, keyed the same as var.api_management_api_operations"
+  value       = { for k, v in azurerm_api_management_api_operation.api_management_api_operations : k => v.method }
 }
 output "api_management_api_operations_operation_id" {
-  description = "List of operation_id values across all api_management_api_operations"
-  value       = [for k, v in azurerm_api_management_api_operation.api_management_api_operations : v.operation_id]
+  description = "Map of operation_id values across all api_management_api_operations, keyed the same as var.api_management_api_operations"
+  value       = { for k, v in azurerm_api_management_api_operation.api_management_api_operations : k => v.operation_id }
 }
 output "api_management_api_operations_request" {
-  description = "List of request values across all api_management_api_operations"
-  value       = [for k, v in azurerm_api_management_api_operation.api_management_api_operations : v.request]
+  description = "Map of request values across all api_management_api_operations, keyed the same as var.api_management_api_operations"
+  value       = { for k, v in azurerm_api_management_api_operation.api_management_api_operations : k => v.request }
 }
 output "api_management_api_operations_resource_group_name" {
-  description = "List of resource_group_name values across all api_management_api_operations"
-  value       = [for k, v in azurerm_api_management_api_operation.api_management_api_operations : v.resource_group_name]
+  description = "Map of resource_group_name values across all api_management_api_operations, keyed the same as var.api_management_api_operations"
+  value       = { for k, v in azurerm_api_management_api_operation.api_management_api_operations : k => v.resource_group_name }
 }
 output "api_management_api_operations_response" {
-  description = "List of response values across all api_management_api_operations"
-  value       = [for k, v in azurerm_api_management_api_operation.api_management_api_operations : v.response]
+  description = "Map of response values across all api_management_api_operations, keyed the same as var.api_management_api_operations"
+  value       = { for k, v in azurerm_api_management_api_operation.api_management_api_operations : k => v.response }
 }
 output "api_management_api_operations_template_parameter" {
-  description = "List of template_parameter values across all api_management_api_operations"
-  value       = [for k, v in azurerm_api_management_api_operation.api_management_api_operations : v.template_parameter]
+  description = "Map of template_parameter values across all api_management_api_operations, keyed the same as var.api_management_api_operations"
+  value       = { for k, v in azurerm_api_management_api_operation.api_management_api_operations : k => v.template_parameter }
 }
 output "api_management_api_operations_url_template" {
-  description = "List of url_template values across all api_management_api_operations"
-  value       = [for k, v in azurerm_api_management_api_operation.api_management_api_operations : v.url_template]
+  description = "Map of url_template values across all api_management_api_operations, keyed the same as var.api_management_api_operations"
+  value       = { for k, v in azurerm_api_management_api_operation.api_management_api_operations : k => v.url_template }
 }
 
